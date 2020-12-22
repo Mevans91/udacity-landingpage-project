b590e4c53acb8ac895e913271b4886809bfe281e
Mevans91 <dragonscale_1@hotmail.com>
2020-12-21 21:47:12 -0600

    removed update button/nav updates dynamically

--- js/app.js
+++ js/app.js
@@ -20,8 +20,8 @@ sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus
 imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam 
 porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et 
 elementum non, faucibus vitae elit. Integer nec libero venenatis libero 
-ultricies molestie semper in tellus. Sed congue et odio sed euismod.
-Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum 
+ultricies molestie semper in tellus. Sed congue et odio sed euismod.`;
+const dummyText2 = `Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum 
 lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae 
 rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse 
 imperdiet porttitor tortor, eget elementum tortor mollis non.`
@@ -33,20 +33,17 @@ imperdiet porttitor tortor, eget elementum tortor mollis non.`
  * Start Helper Functions
  * 
 */
-// function checkSection() {
-//     const sect = document.getElementById(`section${i}`);
-//     if (sect === true) {
-//         return
-//     } else {
-//         break;
-//         console.log('break')
-//     };
-// }
 
-
+/**
+ * The createSection function uses the respectively named functions
+ * to build each piece of the sections, append them up into each other,
+ * and finally build a complete section that matches the ones in the HTML.
+ * It then appends the complete section into the main body container. Finally, 
+ * it updates the nav to reflect the current number and names of sections.  
+ */
 
 function createSection() {
-    const i = document.getElementsByTagName('div').length + 1;
+    const i = document.getElementsByTagName('section').length + 1;
     const sect = document.createElement('section');
     sect.setAttribute('id', `section${i}`);
     sect.setAttribute('data-nav', `Section ${i}`);
@@ -54,7 +51,8 @@ function createSection() {
     createContainer(i);
     createHeader(i);
     createPara(i);
-    
+    createPara2(i);
+    updateNav();
     console.log(i)
 
 }
@@ -80,10 +78,24 @@ function createPara(i) {
 
 }
 
-function createList() {
-    
+function createPara2(i) {
+    const para2 = document.createElement('p');
+    const node3 = document.createTextNode(dummyText2);
+    para2.appendChild(node3);
+    document.getElementById(`section${i}`).querySelector('div').appendChild(para2);
 }
 
+/**
+ * This updateNav function simply grabs the ul by it's id,
+ * clears the contents by setting the inner HTML to an empty string,
+ * and then runs the navBuild function again, updating the list with all 
+ * the current sections.
+ */
+function updateNav() {
+    const nav = document.getElementById('navbarList');
+    nav.innerHTML = '';
+    navBuild();
+}
 
 /**
  * End Helper Functions
@@ -92,23 +104,30 @@ function createList() {
 */
 
 // build the nav
+
+/**
+ * This navBuild function grabs the unordered list by it's id,
+ * and all the sections by their tag name. It then loops through
+ * all the current sections and creates a list item for each section,
+ * adding all the needed attributes and information, before appending 
+ * them all into the list. 
+ */
 function navBuild() {
-    const nav = document.getElementById("navbar__list");
-    const i = 1;
-    checkSection();
-    for (let x = 0; x <= i; x++) {
-        const section = document.getElementById(`section${i}`);
-        console.log(section);
-        // nav.appendChild(section);
-    }
-    
+    const nav = document.getElementById('navbarList');
+    const sections = document.getElementsByTagName('section');
+    for (let x = 1; x <= sections.length; x++) {
+        const item = document.createElement('li');
+        const node = document.createTextNode(`Go To Section ${x}`);
+        // const anchor = document.getElementById(`section${x}`);
+        item.appendChild(node);
+        item.setAttribute('class', 'menuLink');
+        item.setAttribute('onclick', `section${x}.scrollIntoView()`);
+        nav.appendChild(item);
+    } 
 }
 
-
-
-// navBuild();
-createSection();
-
+// createSection();
+navBuild();
 
 // Add class 'active' to section when near top of viewport
 
